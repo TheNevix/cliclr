c9419f3140031fa0da86af5d102bd9fc7cff08d4
TheNevix <ducelliersaimen@gmail.com>
2024-05-19 22:46:44 +0200

    Added function to print bold colored text.

--- src/console_line.rs
+++ src/console_line.rs
@@ -8,8 +8,23 @@ pub struct ConsoleLine{
 }
 
 impl ConsoleLine{
+    
+    /// Prints colored text to the standard output.
+    /// 
+    /// # Parameters
+    /// - `stdout`: A mutable reference to a `StandardStream` to handle colored output.
     pub fn print_colored_text(&self, stdout: &mut StandardStream){
         stdout.set_color(ColorSpec::new().set_fg(Some(self.color))).unwrap();
+        println!("{}", &self.text);
+        stdout.reset().unwrap();
+    }
+
+    /// Prints colored text that is bold to the standard output.
+    /// 
+    /// # Parameters
+    /// - `stdout`: A mutable reference to a `StandardStream` to handle colored output.
+    pub fn print_bold_text(&self, stdout: &mut StandardStream){
+        stdout.set_color(ColorSpec::new().set_fg(Some(self.color)).set_bold(true)).unwrap();
         println!("{}", &self.text);
         stdout.reset().unwrap();
     }
@@ -28,6 +43,7 @@ mod tests {
             color: termcolor::Color::Red, // Example color
         };
         console_line.print_colored_text(&mut stdout);
+        console_line.print_bold_text(&mut stdout);
         // Add a read operation to prevent the terminal from closing immediately
         let mut input = String::new();
         std::io::stdin().read_line(&mut input).expect("Failed to read line");
