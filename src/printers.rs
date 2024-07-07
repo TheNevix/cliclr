e00ba3d6d7cc346671555d42d584dbee2879f604
TheNevix <ducelliersaimen@gmail.com>
2024-07-07 16:24:45 +0200

    Added a test for each printer function.

--- src/printers.rs
+++ src/printers.rs
@@ -1,5 +1,6 @@
 use termcolor::{Color, ColorSpec, StandardStream, WriteColor};
 use crate::console_line::ConsoleLine;
+use std::io::Write;
 
 /// Prints colored text to the standard output.
 /// 
@@ -8,7 +9,7 @@ use crate::console_line::ConsoleLine;
 /// - `line`: A reference to a `ConsoleLine`.
 pub fn print_colored_text(line: &ConsoleLine, stdout: &mut StandardStream){
     stdout.set_color(ColorSpec::new().set_fg(Some(line.color))).unwrap();
-    println!("{}", &line.text);
+    writeln!(stdout, "{}", &line.text).unwrap();
     stdout.reset().unwrap();
 }
 
@@ -19,11 +20,11 @@ pub fn print_colored_text(line: &ConsoleLine, stdout: &mut StandardStream){
 /// - `line`: A reference to a `ConsoleLine`.
 pub fn print_bold_text(line: &ConsoleLine, stdout: &mut StandardStream){
     stdout.set_color(ColorSpec::new().set_fg(Some(line.color)).set_bold(true)).unwrap();
-    println!("{}", &line.text);
+    writeln!(stdout, "{}", &line.text).unwrap();
     stdout.reset().unwrap();
 }
 
-/// Prints colored text that has a specefied background color to the standard output.
+/// Prints colored text that has a specified background color to the standard output.
 /// 
 /// # Parameters
 /// - `line`: A reference to a `ConsoleLine`.
@@ -31,6 +32,41 @@ pub fn print_bold_text(line: &ConsoleLine, stdout: &mut StandardStream){
 /// - `background_color`: A color from termcolor to set the background color of the output.
 pub fn print_background_color_text(line: &ConsoleLine, stdout: &mut StandardStream, background_color: Color){
     stdout.set_color(ColorSpec::new().set_fg(Some(line.color)).set_bg(Some(background_color))).unwrap();
-    println!("{}", &line.text);
+    writeln!(stdout, "{}", &line.text).unwrap();
     stdout.reset().unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    use termcolor::ColorChoice;
+    use super::*;
+    #[test]
+    pub fn test_print_background_color_text(){
+        let mut stdout = StandardStream::stdout(ColorChoice::Always);
+        let console_line = ConsoleLine {
+            text: String::from("A first test."),
+            color: Color::Cyan,
+        };
+        print_background_color_text(&console_line, &mut stdout, Color::Magenta);
+    }
+
+    #[test]
+    pub fn test_print_bold_text(){
+        let mut stdout = StandardStream::stdout(ColorChoice::Always);
+        let console_line = ConsoleLine {
+            text: String::from("A second test."),
+            color: Color::Cyan,
+        };
+        print_bold_text(&console_line, &mut stdout);
+    }
+
+    #[test]
+    pub fn test_print_colored_text(){
+        let mut stdout = StandardStream::stdout(ColorChoice::Always);
+        let console_line = ConsoleLine {
+            text: String::from("A third test."),
+            color: Color::Yellow,
+        };
+        print_colored_text(&console_line, &mut stdout);
+    }
 }
